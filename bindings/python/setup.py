946e729c6150704bd782f640da45628bd2313cc8
caomengxuan666 <2507560089@qq.com>
2026-02-21 20:47:00 +0800

    fix(python): add magic_enum include path to setup.py
    The gossip_core.hpp header includes magic_enum/magic_enum.hpp, but the
    setup.py was missing the include path for this third_party dependency.
    This caused compilation failures in cibuildwheel when building Python wheels.

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -20,7 +20,8 @@ ext_modules = [
             "../../include",
             "../../src",
             "../../third_party/asio/asio/include",
-            "../../third_party/json/single_include/nlohmann"
+            "../../third_party/json/single_include/nlohmann",
+            "../../third_party/magic_enum"
         ],
         cxx_std=17,
         define_macros=[("LIBGOSSIP_BUILD", None)],
