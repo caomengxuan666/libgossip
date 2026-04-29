b8e98341ea4b52769bacd7525ebd829033448b7f
caomengxuan666 <2507560089@qq.com>
2026-04-30 02:20:59 +0800

    chore: remove scripts from version control (user-local only)

--- scripts/build_msvc.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
-cmake --preset=msvc-debug
-cmake --build --preset=msvc-debug
--- scripts/check_build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
-ninja -C C:\Users\cmx\repo\libgossip\build\msvc-debug 2>&1
